d256ca525f81350b662d16994a7411a0ab59eae1
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-12-01 11:21:29 -0800

    Add global-error program

---
+++ chapter03/global-error/src/main.rs
@@ -0,0 +1,29 @@
+extern crate rand;
+
+use rand::{thread_rng, Rng};
+
+static mut ERROR: isize = 0;
+
+struct File;
+
+#[allow(unused_variables)]
+fn read(f: &File, save_to: Vec<u8>) -> usize {
+    // if thread_rng().gen_bool(1.0 / 10_000.0) {
+    if thread_rng().gen_bool(1.0) {
+        unsafe {
+            ERROR = 1;
+        }
+    }
+    0
+}
+#[allow(unused_mut)]
+fn main() {
+    let mut f = File;
+    let mut buffer = vec![];
+    read(&f, buffer);
+    unsafe {
+        if ERROR != 0 {
+            panic!("An error has occurred!")
+        }
+    }
+}
