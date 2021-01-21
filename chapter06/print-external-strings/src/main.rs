fb5e84860c9f1c66781aee9faab8bfd14e216e04
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2021-01-20 20:31:26 -0800

    Add program to print strings from external sources

---
+++ chapter06/print-external-strings/src/main.rs
@@ -0,0 +1,30 @@
+use std::borrow::Cow;
+use std::ffi::CStr;
+use std::os::raw::c_char;
+use std::slice;
+use std::str;
+
+static B: [u8; 10] = [99, 97, 114, 114, 121, 116, 111, 119, 101, 108];
+static C: [u8; 11] = [116, 104, 97, 110, 107, 115, 102, 105, 115, 104, 0];
+
+fn main() {
+    let a = 42;
+    let b: String;
+    let c: Cow<str>;
+
+    unsafe {
+        let b_ptr = &B as *const u8 as *mut u8;
+        // b = String::from_raw_parts(b_ptr, 10, 10);
+        // Causes:
+        // print-external-strings(28853,0x118e455c0) malloc: *** error for object 0x10a0e27e0: pointer being freed was not allocated
+        // print-external-strings(28853,0x118e455c0) malloc: *** set a breakpoint in malloc_error_break to debug
+        let slice = slice::from_raw_parts(b_ptr, 10);
+        // b = str::from_utf8_unchecked(slice).to_string();
+        b = String::from(str::from_utf8_unchecked(slice));
+
+        let c_ptr = &C as *const u8 as *const c_char;
+        c = CStr::from_ptr(c_ptr).to_string_lossy();
+    }
+
+    println!("a: {}, b: {}, c: {}", a, b, c);
+}
