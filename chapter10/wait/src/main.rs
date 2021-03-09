6b3705e438af608b5e217e0cf1108daf3708a80f
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2021-03-08 18:50:49 -0800

    Modify wait program to spawn two threads

--- chapter10/wait/src/main.rs
+++ chapter10/wait/src/main.rs
@@ -2,13 +2,19 @@ use std::{thread, time};
 
 fn main() {
     let start = time::Instant::now();
+    let pause = time::Duration::from_millis(300);
 
-    let handle = thread::spawn(|| {
+    let handle_1 = thread::spawn(move || {
+        thread::sleep(pause);
+    });
+
+    let handle_2 = thread::spawn(move || {
         let pause = time::Duration::from_millis(300);
         thread::sleep(pause);
     });
 
-    handle.join().unwrap();
+    handle_1.join().unwrap();
+    handle_2.join().unwrap();
 
     let finish = time::Instant::now();
 
